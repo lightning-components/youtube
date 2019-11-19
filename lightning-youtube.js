ec466d008364428df331b2ca50d1b8c3710f851d
Justin Fortunato <jfortunato25@gmail.com>
2019-11-19 09:21:07 -0500

    change the usage so that the original iframe & all attributes can be used

--- lightning-youtube.js
+++ lightning-youtube.js
@@ -1,16 +1,47 @@
 class LightningYoutube extends HTMLElement {
+    static REGEX = new RegExp('https:\/\/www\.youtube\.com\/embed\/([a-zA-Z0-9]+)');
     static DEFAULT_WIDTH = 560;
     static DEFAULT_HEIGHT = 315;
 
     constructor() {
         super();
 
-        // encode untrusted values
-        this.videoId = encodeURIComponent(this.dataset.videoid);
-        this.width = +encodeURIComponent(this.dataset.width) || LightningYoutube.DEFAULT_WIDTH;
-        this.height = +encodeURIComponent(this.dataset.height) || LightningYoutube.DEFAULT_HEIGHT;
+        this.assertNoscriptIsOnlyChildNode();
+        this.assertValidYoutubeSource();
 
+        // here, we are going to store the exact iframe supplied to us, so that we can
+        // use it later when we replace the placeholder image with the actual iframe
+        const template = document.createElement('template');
+        template.innerHTML = this.querySelector('noscript').innerHTML.trim();
+        this.iframe = template.content.firstChild;
+
+        // we don't need the noscript or iframe anymore
+        this.innerHTML = '';
+
+        // pull the video id from the iframe src
+        const matches = LightningYoutube.REGEX.exec(this.iframe.src);
+        this.videoId = matches[1];
+        this.width = this.iframe.width || LightningYoutube.DEFAULT_WIDTH;
+        this.height = this.iframe.height || LightningYoutube.DEFAULT_HEIGHT;
         this.attachShadow({mode: 'open'}).innerHTML = this.template();;
+    }
+
+    /**
+     * We require a noscript tag because it will ensure that the iframe never attempts to begin rendering while
+     * still enabling us to use the full iframe tag that the developer would have used without lightning-youtube.
+     * Allowing a playable video with javascript disabled is not a goal of this project because the YouTube iframe
+     * will not work without javascript.
+     */
+    assertNoscriptIsOnlyChildNode() {
+        if (this.children.length !== 1 || this.children[0].tagName !== 'NOSCRIPT') {
+            throw new Error('You must supply a noscript tag. Please refer to the usage documentation for the lightning-youtube component.');
+        }
+    }
+
+    assertValidYoutubeSource() {
+        if (!LightningYoutube.REGEX.test(this.innerHTML)) {
+            throw new Error('You must supply a valid YouTube src. Please refer to the usage documentation for the lightning-youtube component.');
+        }
     }
 
     connectedCallback() {
@@ -58,9 +89,31 @@ class LightningYoutube extends HTMLElement {
 
     insertIframe() {
         const container = this.shadowRoot.querySelector('.lightning-youtube');
-        container.innerHTML = `<iframe class="lightning-youtube__iframe" width="${this.width}" height="${this.height}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen src="https://www.youtube.com/embed/${this.videoId}?autoplay=1"></iframe>`;
+        container.innerHTML = this.getIframeStringWithAutoplayEnabled();
         // we only need to insert the iframe once
         this.removeEventListener('click', this.insertIframe);
+    }
+
+    /**
+     * We already have the original iframe element stored in this.iframe,
+     * but we need to replicate it with all attributes except using
+     * autoplay=1 whether it was developer-supplied or not.
+     */
+    getIframeStringWithAutoplayEnabled() {
+        // here, we ensure that autoplay is enabled because the user has already
+        // indicated that the want the video to play
+        const url = new URL(this.iframe.src);
+        url.searchParams.set('autoplay', '1');
+        this.iframe.src = url.toString();
+
+        // add our own class to the iframe
+        this.iframe.classList.add('lightning-youtube__iframe')
+
+        // ensure the iframe is the same dimensions as the placeholder image
+        this.iframe.width = this.width;
+        this.iframe.height = this.height;
+
+        return this.iframe.outerHTML;
     }
 }
 
